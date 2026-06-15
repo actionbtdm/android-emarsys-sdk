e6b724e639c28c9535ae93da675e711d8a047bfa
filipepereiradeveloper <filipepereiradeveloper@gmail.com>
2026-06-15 11:18:03 +0100

    chore: Remove CI workflows and scripts not relevant for JitPack fork

--- release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export EXCLUDE_GOOGLE_SERVICES_API_KEY=true
-echo Releasing with version: $1
-git tag -a $1 -m $1
-git push --tags
--- revoke.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo Revoke relese with version: $1
-
-git tag -d $1
-git push --delete origin $1
